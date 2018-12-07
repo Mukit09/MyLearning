7247c39c1fb8b90c6664268cea18ec0a73df3ddc
Mukit09 <mukitmkbs25@yahoo.com>
2018-12-07 22:11:54 +0600

    two files deleted

--- MySQL/The-pads.sql
+++
@@ -1,2 +0,0 @@
-select concat(name, '(', left(occupation, 1), ')') from OCCUPATIONS order by name;
-select concat('There are a total of ', count(occupation), ' ', lower(occupation), 's.') from OCCUPATIONS group by occupation order by count(occupation), occupation;
