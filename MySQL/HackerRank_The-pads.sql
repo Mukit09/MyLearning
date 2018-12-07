bcbf19d1fa00d06307a8e48814126eeb6f19f890
Mukit Chowdhury <mukitmkbs25@yahoo.com>
2018-12-07 21:59:40 +0600

    renamed file

---
+++ MySQL/HackerRank_The-pads.sql
@@ -0,0 +1,2 @@
+select concat(name, '(', left(occupation, 1), ')') from OCCUPATIONS order by name;
+select concat('There are a total of ', count(occupation), ' ', lower(occupation), 's.') from OCCUPATIONS group by occupation order by count(occupation), occupation;
