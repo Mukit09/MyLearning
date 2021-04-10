dde17293913d796f516cd030c7ec6afc93f45908
Mukit09 <mukitmkbs25@gmail.com>
2021-04-11 00:19:51 +0600

    unused code removed

--- Java/Java11/module/ModuleService/service/service/a/L.java
+++ Java/Java11/module/ModuleService/service/service/a/L.java
@@ -3,13 +3,3 @@ package service.a;
 public interface L {
 	void eat();
 }
-
-/*
-
-javac --module-path . -d service service/service/a/*.java service/module-info.java
-javac --module-path . -d provider provider/provider/b/*.java provider/module-info.java
-javac --module-path . -d application application/application/c/*.java application/module-info.java
-
-java --module-path . -m application/application.c.Main
-
-*/
